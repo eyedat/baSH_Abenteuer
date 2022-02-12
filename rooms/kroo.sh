a19768c629514f40a2e461fd18de11dfaccb262d
eyeDAT <eyedat@gmail.com>
2022-02-12 21:15:25 +0100

    Add files via upload

---
+++ rooms/kroo.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+clear
+
+# This room gets a little artsy with sleep commands, to help with the
+# narrative of the story. This is why there are two versions - foyer and foyer2.
+
+# Initialise the Title Art
+file1="../art/titleart.ben"
+while IFS= read -r line
+do
+    echo "$line"
+done <"$file1"
+echo
+
+# It's script time again...
+sleep 1
+echo "Was. Der. Tatsächlich. Oh MANN...."
+echo
+sleep 3
+echo "Du hast die Statue eines wunderschönen Kätzchens umarmt. Wie dem auch sei...."
+echo
+echo "Du hast nicht erwartet, dass es zum Leben erweckt und transportiert wird"
+echo "Sie in einen anderen Mystery Room. Das wird etwas seltsam."
+echo
+echo "Sie scheinen sich jetzt in einem kleineren Korridor zu befinden. Sie können"
+echo "einen Schein sehen, der von den Räumen aus Osten und Westen kommt, und"
+echo "südlich von dir ist eine große, alt aussehende Tür."
+echo
+echo "Und was machst du jetzt?"
+
+# And once again the room logic.
+
+while true; do
+    read -p "> " nsewuh
+    case $nsewuh in
+        n ) echo "Du bepflanzt den Wand. Dummkopf." ;;
+        s ) ./bigroom.sh
+             exit ;;
+        e ) ./gameroom.sh
+            exit ;;
+        w ) ./grue.sh
+            exit ;;
+		u ) echo "Hier gibt es nichts, was du verwenden könntest." ;;
+		h ) echo "Nachdem du diese Katze umarmt hast, bist du dir nicht sicher, ob du versuchen solltest, dich noch einmal zu umarmen." ;;
+        * ) echo "Es tut mir leid, ich verstehe dich nicht. Befehle sind: n, e, s, w, u und h. ";;
+    esac
+done
+
+esac
+exit
---
+++ rooms/red.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+clear
+# Initialise the Title Art
+file1="../art/titleart.ben"
+while IFS= read -r line
+do
+    echo "$line"
+done <"$file1"
+echo
+
+# Set up the script for this room. It's a simple one!
+sleep 1
+echo "Du bist in einem Raum, der einen seltsamen roten Schimmer hat."
+echo "Bücherregale säumen die Wände – verstaubte Bände mit Titeln"
+echo "kann nicht recht erkennen. Irgendwie wirken sie uralt."
+echo
+echo "In der Ecke steht ein sehr bequem aussehender Stuhl."
+echo "Der einzige Ausgang befindet sich im Westen, zurück in die Richtung, aus der Sie gekommen sind."
+echo
+echo "Und was nun?"
+
+# And the choices go here.
+while true; do
+    read -p "> " nsewuh
+    case $nsewuh in
+        n ) echo "Gesicht, Wand treffen. Wand, triff Face." ;;
+        s ) echo "Man kann nicht durch Wände gehen." ;;
+        e ) echo "Hier nichts als Mauer." ;;
+        w ) ./mainroom.sh
+            exit ;;
+		u ) echo "Sie sitzen in dem bequemen Sessel. Es ist, als würde man auf einer Wolke sitzen." ;;
+		h ) echo "Du umarmst dich selbst und hoffst, dass die Bücher dich nicht verurteilen." ;;
+        * ) echo "Es tut mir leid, ich verstehe dich nicht. Befehle sind: n, e, s, w, u und h.";;
+    esac
+done
+
+esac
+exit
